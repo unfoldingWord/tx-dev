4b1709bb9787fb80e9c6cf6b7824c46a81428c92
Richard Mahn <rich.mahn@unfoldingword.org>
2021-02-03 16:01:15 -0500

    Updates

--- testNetwork.sh
+++ testNetwork.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-containers=("D43_DevJob_Handler" "tx-enqueue-job_txenqueue_1" "tx-enqueue-job_proxy_1" "obs-pdf" "door43-enqueue-job_proxy_1" "door43-enqueue-job_redis_1" "tX_Dev_HTML_Job_Handler" "door43-enqueue-job_enqueue_1")
+containers=("D43_DevJob_Handler" "tx-enqueue-job_txenqueue_1" "tx-enqueue-job_proxy_1" "obs-pdf" "uw-pdf" "door43-enqueue-job_proxy_1" "door43-enqueue-job_redis_1" "tX_Dev_HTML_Job_Handler" "door43-enqueue-job_enqueue_1")
 
 for container in "${containers[@]}";
 do
