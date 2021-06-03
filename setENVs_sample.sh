a5205385bb25ead4aa1b956451ac9a4388b0c480
Richard Mahn <richmahn@users.noreply.github.com>
2021-06-03 16:46:45 -0400

    Adds AWS endpoints
    Former-commit-id: 443cff22acdc9d04d5d2acedef9140cdf02de7af
    Former-commit-id: 37c7cc388f246c0cb3691b77c4df2808c2001a44

--- setENVs_sample.sh
+++ setENVs_sample.sh
@@ -4,6 +4,10 @@
 export AWS_ACCESS_KEY_ID="AKJ.........QRF"
 export AWS_SECRET_ACCESS_KEY="kxZ...................1bm"
 
+# OPTIONAL
+export DCS_PORT=5555
+export DOOR43_PREIVEW_PORT=5556
+
 # DO NOT CHANGE
 export DB_ENDPOINT="door43.cluster-ccidwldijq9p.us-west-2.rds.amazonaws.com"
 export TX_DATABASE_PW="fxt......bv1"
@@ -11,6 +15,6 @@ export GOGS_USER_TOKEN="672................................882"
 export GITEA_URL="http://dcs"
 export RESTRICT_GITEA_URL="false"
 export QUEUE_PREFIX="dev-"
-export REDIS_URL="redis://door43-enqueue-job_redis_1:6379"
+export REDIS_URL="redis://redis:6379"
 export DEBUG_MODE="true"
 export TEST_MODE="true"
