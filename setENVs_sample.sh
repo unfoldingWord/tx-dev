9b4bc5f43b1e7528d2f7f7cfd1b9fd7ca8dd8b3f
Richard Mahn <richmahn@users.noreply.github.com>
2021-06-04 08:18:00 -0400

    Adds aws_endpoint_url to all services
    Former-commit-id: 650b5feaab6106226bf50ea9c391da65c4dc2222
    Former-commit-id: 583c6e2bc9eefc93be908f671cf2612a95f72218

--- setENVs_sample.sh
+++ setENVs_sample.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-# REPLACE THESE WITH YOURS
-export AWS_ACCESS_KEY_ID="AKJ.........QRF"
-export AWS_SECRET_ACCESS_KEY="kxZ...................1bm"
-
 # OPTIONAL
 export DCS_PORT=5555
 export DOOR43_PREIVEW_PORT=5556
 
 # DO NOT CHANGE
-export DB_ENDPOINT="door43.cluster-ccidwldijq9p.us-west-2.rds.amazonaws.com"
-export TX_DATABASE_PW="fxt......bv1"
-export GOGS_USER_TOKEN="672................................882"
+export AWS_ACCESS_KEY_ID="123"
+export AWS_SECRET_ACCESS_KEY="123"
+export DB_ENDPOINT="sqlite:///:memory:"
+export TX_DATABASE_PW="123"
+#export DB_ENDPOINT="door43.cluster-ccidwldijq9p.us-west-2.rds.amazonaws.com"
+#export TX_DATABASE_PW="fxt......bv1"
+export GOGS_USER_TOKEN="123"
 export GITEA_URL="http://dcs"
 export RESTRICT_GITEA_URL="false"
 export QUEUE_PREFIX="dev-"
 export REDIS_URL="redis://redis:6379"
 export DEBUG_MODE="true"
 export TEST_MODE="true"
+export AWS_ENDPOINT_URL="http://s3:4566"
