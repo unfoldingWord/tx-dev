0c940302ce338d8d911be4303b6c2fd3a83c2c21
Richard Mahn <richmahn@users.noreply.github.com>
2022-01-03 18:22:11 -0500

    Updates .gitmodules

--- setup.sh
+++ setup.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-if [[ ! -e setENVs.sh ]]; then
-  cp setENVs_sample.sh setENVs.sh
-fi
-
 if [[ ! -e .env ]]; then
   cp env_sample .env
 fi
 
-npm ci
-
 git submodule update --init --recursive
 
-#tar -xf dcs/data.tar.gz -C dcs
+cd tx-dev-dcs
+bash ./untar_data.sh
 
